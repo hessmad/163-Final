33ea24edd0233fc41b90ab51a30487288afe7f57
hessmad <37817650+hessmad@users.noreply.github.com>
2019-06-06 17:59:52 -0700

    Parse dataframes

--- final.py
+++ final.py
@@ -1,41 +1,40 @@
 # Final Project
 
 import pandas as pd
+import geopandas as gpd
 
 # Load in data
+geo_data = gpd.read_file('United States Counties.csv')
+food_access = pd.read_excel('access2015.xls', sheet_name='ACCESS')
+stores = pd.read_excel('access2015.xls', sheet_name='STORES')
+restaurants = pd.read_excel('access2015.xls', sheet_name='RESTAURANTS')
+
 pop_by_county = pd.read_excel('access2015.xls',
                               sheet_name='Supplemental Data - County')
 part_by_state = pd.read_excel('access2015.xls',
                               sheet_name='Supplemental Data - State')
-obes_prev = pd.read_excel('obs.xlsx')
 exp_per_cap = pd.read_csv('health_exp_per_capita.csv')
 
-# Select rows of interst for the obesity prevalence data
-obes_prev.drop([('number04', 'percent04', 'lower confidence limit04',
-                 'upper confidence limit04', 'age-adjusted percent04',
-                 'age-adjusted lower confidence limit04',
-                 'age-adjusted upper confidence limit04', 'number05',
-                 'percent05', 'lower confidence limit05',
-                 'upper confidence limit05', 'age-adjusted percent05',
-                 'age-adjusted lower confidence limit05',
-                 'age-adjusted upper confidence limit05', 'number06',
-                 'percent06', 'lower confidence limit06',
-                 'upper confidence limit06', 'age-adjusted percent06',
-                 'age-adjusted lower confidence limit06',
-                 'age-adjusted upper confidence limit06', 'number07',
-                 'percent07', 'lower confidence limit07',
-                 'upper confidence limit07', 'age-adjusted percent07',
-                 'age-adjusted lower confidence limit07',
-                 'age-adjusted upper confidence limit07', 'number08',
-                 'percent08', 'lower confidence limit08',
-                 'upper confidence limit08', 'age-adjusted percent08',
-                 'age-adjusted lower confidence limit08',
-                 'age-adjusted upper confidence limit08', 'number09',
-                 'percent09', 'lower confidence limit09',
-                 'upper confidence limit09', 'age-adjusted percent09',
-                 'age-adjusted lower confidence limit09',
-                 'age-adjusted upper confidence limit09')], axis=1,
-               inplace=True)
+# Select rows of interest in food access dataframe
+food_access = food_access[['FIPS', 'State', 'County','PCT_LACCESS_POP10',
+                           'PCT_LACCESS_LOWI10', 'PCT_LACCESS_CHILD10', 
+                           'PCT_LACCESS_SENIORS10', 'PCT_LACCESS_HHNV10']]
+
+# Select rows of interest in stores dataframe
+stores = stores[['PCH_GROC_07_12', 'PCH_GROCPTH_07_12', 'PCH_SUPERC_07_12',
+                 'SUPERCPTH07', 'SUPERCPTH12', 'PCH_SUPERCPTH_07_12', 
+                 'PCH_CONVS_07_12', 'CONVSPTH07', 'CONVSPTH12',
+                 'PCH_CONVSPTH_07_12', 'PCH_SPECS_07_12', 'SPECSPTH07',
+                 'SPECSPTH12', 'PCH_SPECSPTH_07_12', 'PCH_SNAPS_08_12',
+                 'SNAPSPTH08', 'SNAPSPTH12', 'PCH_SNAPSPTH_08_12']]
+
+# Select rows of interest in restaurants dataframe
+restaurants = restaurants[['PCH_FFR_07_12', 'FFRPTH07', 'FFRPTH12',
+                           'PCH_FFRPTH_07_12', 'PC_FFRSALES02',
+                           'PC_FFRSALES07', 'PC_FSRSALES02', 'PC_FSRSALES07']]
+
+
+# Select rows of interest in participation dataframe
 
 # Select rows of interest for the health expenditure data
 exp_per_cap = exp_per_cap[['Item', 'Group', 'Region_Name', 'State_Name',
@@ -48,3 +47,5 @@ exp_per_cap = exp_per_cap.rename(index=str,
                                           'Y2012': 'H_EXP2012',
                                           'Y2013': 'H_EXP2013',
                                           'Y2014': 'H_EXP2014'})
+
+# Join data frames with geometry information
